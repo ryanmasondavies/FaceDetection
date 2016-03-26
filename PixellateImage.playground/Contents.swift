15d26ceb003c22437de66edc2f978ac9dbd89a47
Ryan Davies <mail@ryanjdavies.com>
2016-03-26 20:53:42 +0000

    Update playground to use imageByApplyingFilter

--- PixellateImage.playground/Contents.swift
+++ PixellateImage.playground/Contents.swift
@@ -1,23 +1,16 @@
 import UIKit
 import CoreImage
 
-func pixellate(inputImage: CIImage) -> CIImage? {
-    let imageSize = inputImage.extent.size
-    let pixellationOptions = [
-        kCIInputImageKey: inputImage,
+func pixellate(image image: CIImage) -> CIImage? {
+    let imageSize = image.extent.size
+    let options = [
         kCIInputCenterKey: CIVector(x: imageSize.width / 2, y: imageSize.height / 2),
         kCIInputScaleKey: max(imageSize.width, imageSize.height) / 20,
     ]
-    let pixellation = CIFilter(name: "CIPixellate", withInputParameters: pixellationOptions)
-    
-    guard let pixellatedImage = pixellation?.outputImage else {
-        return nil
-    }
-    
-    return pixellatedImage
+    return image.imageByApplyingFilter("CIPixellate", withInputParameters: options)
 }
 
 let monaLisaURL = NSBundle.mainBundle().URLForResource("monalisa", withExtension: "jpg")
-let image = CIImage(contentsOfURL: monaLisaURL!)
+let monaLisa = CIImage(contentsOfURL: monaLisaURL!)
 
-pixellate(image!)
+pixellate(image: monaLisa!)
